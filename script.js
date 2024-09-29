98ec8884cff10d9567e40248c12539899593a79b
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-29 01:14:40 -0300

    Added else clause to addToCart function, introduced updateCartModal function, and modified cart item rendering

--- script.js
+++ script.js
@@ -53,10 +53,40 @@ function addToCart(name,price){
     if (existingItem) {
         existingItem.quantity += 1
     }
-    
-    cart.push({
-       name,
-       price,
-       quantity: 1
+    else{
+        cart.push({
+           name,
+           price,
+           quantity: 1
+        })
+    }
+
+    updateCartModal()
+}
+
+function updateCartModal(){
+    cartItemsContainer.innerHTML = ""
+    let total = 0
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div")
+       
+        cartItemElement.innerHTML = `
+        <div>
+            <div>
+                <p>${item.name}</p>
+                <p>${item.quantity}</p>
+                <p>R$ ${item.price}</p>
+            </div>
+
+            <div>
+                <button>
+                    Remover
+                </button>
+            </div>
+        </div>
+        
+        `
+        cartItemsContainer.appendChild(cartItemElement)
     })
 }
