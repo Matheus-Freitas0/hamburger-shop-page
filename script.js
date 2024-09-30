4146687ff774e0aa39d9525f0cb7a79ce6239949
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-30 17:08:48 -0300

    Updated script.js and styles/output.css with various code changes and additions, including event listeners, functions, and CSS classes.

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ cartBtn.addEventListener("click", function () {
     cartModal.style.display = "flex"
 })
 
-cartModal.addEventListener("click", function(event){
-    if(event.target === cartModal){
+cartModal.addEventListener("click", function (event) {
+    if (event.target === cartModal) {
         cartModal.style.display = "none"
-        }
+    }
 })
 
-closeModalBtn.addEventListener("click", function(){
+closeModalBtn.addEventListener("click", function () {
     cartModal.style.display = "none"
 })
 
@@ -39,40 +39,40 @@ menu.addEventListener("click", function (event) {
 
     let parentButton = event.target.closest(".add-to-cart-btn")
 
-    if(parentButton){
+    if (parentButton) {
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
 
-        addToCart(name,price)
+        addToCart(name, price)
     }
 })
 
-function addToCart(name,price){
-    
-    const existingItem = cart.find(item =>item.name === name)
+function addToCart(name, price) {
+
+    const existingItem = cart.find(item => item.name === name)
 
     if (existingItem) {
         existingItem.quantity += 1
     }
-    else{
+    else {
         cart.push({
-           name,
-           price,
-           quantity: 1
+            name,
+            price,
+            quantity: 1
         })
     }
 
     updateCartModal()
 }
 
-function updateCartModal(){
+function updateCartModal() {
     cartItemsContainer.innerHTML = ""
     let total = 0
 
     cart.forEach(item => {
         const cartItemElement = document.createElement("div")
         cartItemElement.classList.add("flex", "justiffy-between", "mb-4", "flex-col")
-        
+
         cartItemElement.innerHTML = `
         <div class="flex items-center justify-between">
             <div>
@@ -81,7 +81,7 @@ function updateCartModal(){
                 <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
             </div>
 
-                <button>
+                <button class="remove-cart-btn" data-name="${item.name}">
                     Remover
                 </button>
 
@@ -98,5 +98,48 @@ function updateCartModal(){
     })
 
     cartCounter.innerHTML = cart.length
-    
+
 }
+
+cartItemsContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("remove-cart-btn")) {
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name)
+    }
+})
+
+function removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name)
+
+    if (index !== -1) {
+        const item = cart[index]
+        
+        if (item.quantity > 1) {
+            item.quantity -= 1
+            updateCartModal()
+            return
+        }
+
+        cart.splice(index, 1)
+        updateCartModal()
+    }
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value
+
+    if (inputValue !== "") {
+        addressInput.classList.remove("border-red-500")
+        addressInput.classList.add("hidden")
+    }
+})
+
+checkoutBtn.addEventListener("click", function(){
+    if (cart.length === 0) return
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return
+    }
+})
