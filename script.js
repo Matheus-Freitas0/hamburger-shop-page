dc92ffda438261bbc3c3724637aba01e6428d9ce
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-30 16:09:50 -0300

    Updated cart modal layout and styling

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const addressWarn = document.getElementById("address-warn")
 let cart = []
 
 cartBtn.addEventListener("click", function () {
+    updateCartModal()
     cartModal.style.display = "flex"
 })
 
@@ -70,20 +71,20 @@ function updateCartModal(){
 
     cart.forEach(item => {
         const cartItemElement = document.createElement("div")
-       
+        cartItemElement.classList.add("flex", "justiffy-between", "mb-4", "flex-col")
+        
         cartItemElement.innerHTML = `
-        <div>
+        <div class="flex items-center justify-between">
             <div>
-                <p>${item.name}</p>
-                <p>${item.quantity}</p>
-                <p>R$ ${item.price}</p>
+                <p class="font-bold">${item.name}</p>
+                <p>Qtd: ${item.quantity}</p>
+                <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
             </div>
 
-            <div>
                 <button>
                     Remover
                 </button>
-            </div>
+
         </div>
         
         `
